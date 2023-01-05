6ae80e7c76f59698f63ea3a39d43a4b7249fba72
AliNough <112473635+AliNough@users.noreply.github.com>
2023-01-05 09:57:08 +0100

    lesson v2.0

--- script1.js
+++ script1.js
@@ -14,3 +14,43 @@ let randNum = () => {
 }
 
 randNum();
+
+let pet = {
+    name: 'bro',
+    age: 23,
+};
+
+console.log(pet);
+
+const fName = 'john';
+const lName = 'doe';
+const fullName = fName + ' ' + lName;
+
+console.log(fullName);
+
+
+    
+    let wholeNum = 10;
+let gravity = 9.8;
+
+const weekdays = 'Monday - Tuesday - Wendsday - Thursday - Friday';
+
+let numMulti = 2 * 2;
+let numDiv = 48 / 2;
+let numAdd = 30 + 30;
+let numSub = 90 - 23;
+
+let alligations = false;
+
+console.log('hello world'.length);
+
+var number1 = "53";
+parseInt(number1);
+var number2 = "44.6";
+parseFloat(number2);
+
+if (typeof window !== 'undefined') {
+    document.querySelector('h1').innerHTML = fullName + ' ' + 'heeeeell no';
+    document.querySelector('h1').style = 'background-color: green;';
+}
+
